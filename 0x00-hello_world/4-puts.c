0b814e8e1d4582b163b931a9fc723d984c694e78
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-07-13 11:25:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main functions
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
