b96794376b3baac4080eb0285edb0c0d318d2eee
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-07-21 05:16:08 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers -  prints the numbers, from 0 to 9, followed by a new line
- *
  * Return: Always 0
  */
 
@@ -10,7 +9,7 @@ void print_numbers(void)
 {
 	int n;
 
-	for (n = 0 ; n < 9 ; n++)
+	for (n = 0 ; n < 10 ; n++)
 		_putchar(n + '0');
 	_putchar('\n');
 }
