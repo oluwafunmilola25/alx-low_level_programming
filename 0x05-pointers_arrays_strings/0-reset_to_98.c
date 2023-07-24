214176f971ab0ae6e2cc2aaf70d0136322da0849
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-07-24 07:35:52 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value to 98
+ * @n: parameter that points to the value to be checked
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
