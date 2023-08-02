731137781fc52da8989e4ff345c5ead19ad2439c
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-08-02 16:22:58 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - a function that print prime numbers
+ * @n: number
+ * Return: return 1 if the integer is a prime number or 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 1)
+		return (0);
+	return (actual_prime_number(n, n - 1));
+}
+
+/**
+ * actual_prime_number - checks if a number is a prime number
+ * @n: number
+ * @h: input
+ * Return: return 1 if n is prime or 0 if not
+ */
+
+int actual_prime_number(int n, int h)
+{
+	if (h == 1)
+		return (1);
+	if (n % h == 0 && h > 0)
+		return (0);
+	return (actual_prime_number(n, h - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int actual_sqrt_recursion(int n, int h);
+int actual_prime_number(int n, int h);
 
 #endif
