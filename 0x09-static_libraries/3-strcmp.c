7f4a9306f01eb0e4d445e02a60dd82b312041a23
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-08-04 12:52:17 -0700

    My first  libmy.a commit

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+			a++;
+			if (s1[a] < s2[a])
+				return (s1[a] - s2[a]);
+			else if (s2[a] < s1[a])
+				return (s1[a] - s2[a]);
+		}
+	}
+	return (0);
+}
