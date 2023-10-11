620b5723d5c89e9619301d7b532befdecf8d5115
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-11 05:32:35 -0400

    dog.h commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - a new type to be defined
- *
- * Return: no return value
+ * @name: the name parameter
+ * @age: the age parameter
+ * @owner: the owner parameter
  */
 
 struct dog
@@ -11,5 +13,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
