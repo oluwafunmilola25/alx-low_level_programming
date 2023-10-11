6a3b33a31a634f21da724fbc67994aa168d7dca6
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-11 07:08:46 -0400

    2-print_dog.c commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,10 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->name == NULL || d->owner == NULL)
-	{
-		printf("nil");
-	}
-	else
-	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
