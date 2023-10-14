af166921878a4c871741f848c43bab11b560c633
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-13 20:22:08 -0400

    last number commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *main_ptr = (unsigned char *)main;
+	int i;
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02x", main_ptr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
