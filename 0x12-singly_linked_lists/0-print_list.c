5108c21c530af009a2d6a431fca099beb1c30fc6
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-20 00:43:29 -0400

    0-print_list commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a lists
+ * @h: parameter
+ * Return:the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
