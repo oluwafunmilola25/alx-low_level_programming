e763b3aaf5501fb39072572f605c6c3b4752efec
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-20 02:22:35 -0400

    4 freelist commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all unused list file
+ * @head: singly list parameter
+ * Return: freed list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *unused;
+	list_t *node;
+
+	unused = head;
+
+	while (unused != NULL)
+	{
+		node = unused->next;
+		free(unused);
+		unused = node;
+	}
+}
