2373ea20c7ff1c5f63de2110aa2c00c62327dc51
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-22 11:47:10 -0400

    101-mul commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
-int _strlen(char *s);
+int mul(int num1, int num2);
 int main(int argc, char *argv[]);
 
 #endif
