ba1a3e25836cdfbeb334bd35f543a4ab6fde668a
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-24 10:54:12 -0400

    5-free_listint commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -n frees a listint list
+ * @head: a pointer to the first node in a singly list
+ * Return: success
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
