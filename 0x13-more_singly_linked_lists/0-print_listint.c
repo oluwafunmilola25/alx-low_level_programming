edfb05ff7a655ce8ff8fac53c0cc19daf6181375
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-23 21:44:03 -0400

    0-print listint commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint list
+ * @h: head node
+ * Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
+    return (count);
+}
