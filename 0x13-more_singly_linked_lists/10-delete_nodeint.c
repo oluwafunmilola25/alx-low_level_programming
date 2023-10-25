26287b432628fb613a6544fc1fd1c7da52490921
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-24 22:32:22 -0400

    5-free_listint2 commit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a listint list
+ * @head: a pointer to the first node
+ * @index: index of the node to be deleted
+ * Return: 1 or -1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
