24361ed0b83a14b54f5815642db70b45a6f2554a
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-25 00:58:58 -0400

    102-free_listint_safe commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint linked list
+ * @h: the head
+ * Return: size of the list freed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	int data;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h != NULL)
+	{
+		data = *h - (*h)->next;
+		if (data > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			size++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			size++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (size);
+}
