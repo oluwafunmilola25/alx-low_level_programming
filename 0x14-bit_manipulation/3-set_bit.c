f9a711eab668dfe9d77729469453dada3b5132ed
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-27 00:31:58 -0400

    3-set_bit commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,8 +3,8 @@
 /**
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: number
- * @index: index starting from 0 from 0 of the bit to b set
- * Return: 1 if success and -1 if failed
+ * @index: index starting from 0 of the bit
+ * Return: 1 for success and -1 for failed
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
@@ -12,6 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 	*n = ((1UL << index) | *n);
-	
 	return (1);
 }
