b49a7ba894198075f74f35d4d894191401a2d175
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-10-30 17:00:15 -0400

    2-appends text to file commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends a text at the end of a file
+ * @filename: the name of the file to be appended
+ * @text_content: the content to be appended
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+	int g;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		g = write(fd, text_content, len);
+		if (g != len)
+			return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
