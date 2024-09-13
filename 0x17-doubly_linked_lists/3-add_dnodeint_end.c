a8abebdeca19c9c791e641aff9132556a6a4bd2c
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2024-09-13 14:01:22 -0500

    7-insert_dnodeint.c, 2-add_dnodeint.c, 3-add_dnodeint_end.c commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,6 +6,7 @@
  * @n: value of the element
  * Return: address of the new element
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *h;
@@ -14,16 +15,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
-
 	h = *head;
-
 	if (h != NULL)
 	{
-		while (h->next != NULL)
-			h = h->next;
+		while
+			(h->next != NULL)
+				h = h->next;
 		h->next = new;
 	}
 	else
@@ -31,6 +30,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 	}
 	new->prev = h;
-
 	return (new);
 }
